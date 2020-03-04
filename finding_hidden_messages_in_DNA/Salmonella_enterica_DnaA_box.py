ba3129bca68380f11a2a6201fc99e72c8d82a52b
kiselevskaya <kiselevskaya.viktoriya@gmail.com>
2020-03-04 23:41:49 +0000

    added Salmonella_enterica_DnaA_box file

---
+++ finding_hidden_messages_in_DNA/Salmonella_enterica_DnaA_box.py
@@ -0,0 +1,39 @@
+
+
+import os
+from skew_array import *
+from neighbors import *
+import timeit
+
+
+start = timeit.default_timer()
+
+salmonella = open(os.getcwd()+'\\text_files\\Salmonella_enterica.txt', 'r')
+genome = ''.join(salmonella.readlines()).replace('\n', '')
+salmonella.close()
+
+
+def salmonella_enterica_dnaa_box(genome, k, d):
+    dnaA_box = ''
+    frequency = {}
+    minimum = minimum_skew(genome)
+    print('Minimum skew at position(s):', minimum)
+    check_boxes = [[minimum[0], minimum[0]+500],
+                   [minimum[-1]-500, minimum[-1]],
+                   [sum(minimum)//len(minimum)-250, sum(minimum)//len(minimum)+250]]
+    for l in range(len(check_boxes)):
+        text = genome[check_boxes[l][0]:check_boxes[l][1]]
+        frequent_words = frequent_words_with_mismatches_sorting(text, k, d)
+        count = len(frequent_words)
+        frequency[str(check_boxes[l])] = count
+    for key, value in frequency.items():
+        if value == max(frequency.values()):
+            dnaA_box = key
+    return dnaA_box
+
+
+print(salmonella_enterica_dnaa_box(genome, 9, 1))
+
+
+stop = timeit.default_timer()
+print("Program Executed in", stop-start)
