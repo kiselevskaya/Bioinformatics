cea8cfea041a96b957a07218dc360a788c6e830b
kiselevskaya <kiselevskaya.viktoriya@gmail.com>
2020-03-04 18:15:27 +0000

    added frequent_words_with_mismatches_sorting to neighbors

--- finding_hidden_messages_in_DNA/neighbors.py
+++ finding_hidden_messages_in_DNA/neighbors.py
@@ -1,5 +1,6 @@
 
 
+import timeit
 from hamming_distance import *
 
 
@@ -28,3 +29,36 @@ def neighbors(pattern, d):
         else:
             neighborhood.add(pattern[:1]+suffix)
     return neighborhood
+
+
+def frequent_words_with_mismatches_sorting(text, k, d):
+    frequent_patterns = set()
+    neighborhoods = []
+    indexes = []
+    for i in range(len(text) - k + 1):
+        neighborhoods += (list(neighbors(text[i:i+k], d)))
+    for j in range(len(neighborhoods)):
+        indexes.append(pattern_to_number(neighborhoods[j]))
+    count = [1] * len(indexes)
+    sorted_index = sorted(indexes)
+    for n in range(len(neighborhoods) - 1):
+        if sorted_index[n] == sorted_index[n + 1]:
+            count[n + 1] = count[n] + 1
+    max_count = max(count)
+    for m in range(len(neighborhoods)):
+        if count[m] == max_count:
+            pattern = number_to_pattern(sorted_index[m], k)
+            frequent_patterns.add(pattern)
+    return frequent_patterns
+
+
+start = timeit.default_timer()
+
+text = 'AATCCTCCGGGCGGGTATCTGCCGGGCTGCCGGGCCTCCTGCTATCGGGCTGCCGGGTATTATTATAATTATTATCGGGTATCCTCCTGCAATCTGCTATCCTCCCTCCTGCCTGCTATCGGGTATTATTATAATCTGCCCTCAATCCTCAATCTGCTATCTGCTATCTGCCCTCCCTCCTGCCGGGCGGGAATTATCGGGCGGGCGGGTATCCTCCTGCCGGGCGGGCTGCCTGCTATCTGCAATCTGCCTGCCGGGCGGGTATCTGCCTGCAATCGGGCTGCCCTCCTGCCCTCCCTCAATCCTCTATCTGCCCTCCCTC'
+k, d = 5, 3
+print(frequent_words_with_mismatches_sorting(text, k, d))   # {'CGCCG'}
+
+stop = timeit.default_timer()
+print("Program Executed in", stop-start)
+
+
