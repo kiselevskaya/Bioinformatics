5d12393a2d8430ae6faf44baeeea9a2f16f08ce4
kiselevskaya <kiselevskaya.viktoriya@gmail.com>
2020-08-04 16:47:45 +0100

    modified manhattan_tourist that now can crate path in a cross direction

--- comparing_genes_proteins_genomes/sequence_alignment/manhattan_tourist.py
+++ comparing_genes_proteins_genomes/sequence_alignment/manhattan_tourist.py
@@ -1,6 +1,6 @@
 
 
-def manhattan_tourist(n, m, down, right):
+def manhattan_tourist(n, m, down, right, cross=None):
     '''
     :param n: number of rows
     :param m: number of columns
@@ -20,7 +20,10 @@ def manhattan_tourist(n, m, down, right):
 
     for i in range(1, n+1):
         for j in range(1, m+1):
-            weight_matrix[i][j] = max(weight_matrix[i-1][j] + down[i-1][j], weight_matrix[i][j-1] + right[i][j-1])
+            try:
+                weight_matrix[i][j] = max(weight_matrix[i-1][j] + down[i-1][j], weight_matrix[i][j-1] + right[i][j-1], weight_matrix[i-1][j-1] + cross[i][j])
+            except Exception as e:
+                weight_matrix[i][j] = max(weight_matrix[i-1][j] + down[i-1][j], weight_matrix[i][j-1] + right[i][j-1])
     return weight_matrix[n][m]
 
 
@@ -35,5 +38,5 @@ if __name__ == '__main__':
     n, m = int(input[0].split()[0]), int(input[0].split()[1])
     down = [[int(e) for e in E.split()] for E in input[1:input.index('-')]]
     right = [[int(e) for e in E.split()] for E in input[input.index('-')+1:]]
-
+    print(manhattan_tourist.__doc__)
     print(manhattan_tourist(n, m, down, right))   # 88
